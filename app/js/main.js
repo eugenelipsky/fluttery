e20d21fad01a3f050bbc24db557500e23e7d4081
eugenelipsky <zvlipskiy@mail.ru>
2020-03-12 12:55:38 +0200

    Responsive added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
 
+    $('.menu__list-mobile').on('click', function (event) {
+        event.stopPropagation();
+        $('.menu__list').slideToggle();
+        $(".menu__list").on('click', function (event) {
+            event.stopPropagation();
+        });
+        $(this).toggleClass('active');
+    })
+
+    $(document).on('click', function () {
+        $('.menu__list').hide();
+        $('.menu__list-mobile').removeClass('active');
+    });
+
+
 });
